76e1e774d68afedc392c192549177089473987f0
jakubz00 <kuba.zapletal0@gmail.com>
2024-03-25 02:30:37 +0100

    stable version, testing in gitignore

--- algorithms.py
+++ algorithms.py
@@ -1,15 +1,12 @@
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
-
-
 #Processing data
 class Algorithms:
-    
     def __init__(self):
         pass
-    
-    
+
+
     def analyze_point_polygon_position(self, q:QPointF, pol:QPolygonF):
         #Inicialize amount of intersections
         k = 0
@@ -24,20 +21,15 @@ class Algorithms:
             xi1r = pol[(i+1)%n].x() - q.x()
             yi1r = pol[(i+1)%n].y() - q.y()
             
-            #Suitable segment?
             if ((yi1r > 0) and (yir <= 0)) or ((yir > 0) and (yi1r <= 0)):
-               
                #Compute intersection
                xm = (xi1r * yir - xir * yi1r)/(yi1r - yir)
-               
                #Right half plane
                if xm > 0:       
-                   k += 1  
-                   
+                   k += 1
         #Point q inside polygon?
-        if (k%2 == 1):
+        if k%2 == 1:
             return 1
-        
         #Point q outside polygon
         return 0
 
--- draw.py
+++ draw.py
@@ -2,149 +2,158 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtGui import QMouseEvent, QPaintEvent
 from PyQt6.QtWidgets import *
-from osgeo import ogr
-from os.path import *
-from osgeo import ogr
-import time
-import sys
-
-
+import algorithms
+from shapely.geometry import Polygon
+import geopandas as gpd
+from os import path
 
 class Draw(QWidget):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+        # Inicialize features
         self.q = QPointF(-100, -100)
-        self.pol = QPolygonF()
-        self.add_vertex = True
+        self.add_vertex = False
+        self.polygons = []
+        self.features = [None]
+        self.min_max = [0, 0, 10, 10]
+        self.results = []
 
-    def polygon_to_polygon(geometry):
+
+    def shapely_to_qpolygonf(shape_polygon):
         """
-        Converts geometry to QPolygonF
-        :return: list of polygons
+        Converts a Shapely polygon object to a Qt QPolygonF object.
+        :param Shapely polygon object.
+        :return: A Qt QPolygonF object representing the converted polygon
         """
-        if geometry.GetGeometryName() == 'POLYGON':
-            polygon = QPolygonF()
-            for point in geometry.GetGeometryRef(0):
-                polygon.append(QPointF(point[0], point[1]))
-            return polygon
-        elif geometry.GetGeometryName() == 'MULTIPOLYGON':
-            polygons = []
-            for i in range(geometry.GetGeometryCount()):
-                polygon = QPolygonF()
-                for point in geometry.GetGeometryRef(i).GetGeometryRef(0):
-                    polygon.append(QPointF(point[0], point[1]))
-                polygons.append(polygon)
-            return polygons
+        qpolygon = QPolygonF()
+        for point in shape_polygon.exterior.coords:
+            qpolygon.append(QPointF(point[0], point[1]))
+        return qpolygon
+
+    def resize_polygons_to_widget(self, padding=50):
+        """
+        Resizes all QPolygons in self.polygons to fit proportionally within the widget with padding.
+        :param padding: padding in pixels
+        :type padding int
+        :return: list of QPolygons
+        """
+        # Get widget geometry,
+        x_win = int(self.widget_size.x() + padding)
+        y_win = int(self.widget_size.y() + padding)
+        width_win = int(self.widget_size.width() - padding)
+        height_win = int(self.widget_size.height() - padding)
+        x_min_dat = self.extent[0]
+        x_max_dat = self.extent[2]
+        y_min_dat = self.extent[1]
+        y_max_dat = self.extent[3]
+        resized_polygons = []
+        for feature in self.features:
+            scaled_point_list = []
+            for point in feature:
+                scaled_x = int(((point.x() - x_min_dat) / (x_max_dat - x_min_dat) * width_win))
+                scaled_y = int((height_win - (point.y() - y_min_dat) / (y_max_dat - y_min_dat) * height_win))
+                scaled_point = QPointF(scaled_x, scaled_y)
+                scaled_point_list.append(scaled_point)
+            resized_polygon = QPolygonF(scaled_point_list)
+            resized_polygons.append(resized_polygon)
+        return resized_polygons
+
+    def gis_to_qt_polygons(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open file", "", "")
+        if path.splitext(filename)[1] in ['.gpkg', '.shp', '.geojson']:
+            # Load shapefile using GeoPandas
+            gdf = gpd.read_file(filename)
+            self.extent = [item for item in (gdf.total_bounds)]
+            # Convert Shapely polygons to QPolygonF and store them in self.features
+            self.features = []
+            for shapely_geometry in gdf.geometry:
+                if isinstance(shapely_geometry, Polygon):
+                    self.features.append(Draw.shapely_to_qpolygonf(shapely_geometry))
+                else:
+                    for polygon in shapely_geometry:
+                        self.features.append(Draw.shapely_to_qpolygonf(polygon))
+            self.polygons = Draw.resize_polygons_to_widget(self)
+            self.results = [0] * len(self.polygons)
+
         else:
-            return None
-
-
-    def gis_to_polygon(self, path: str):
-        """
-        Converts geodata to list of QPolygonF objects using OGR
-        :param path: path to file, receiving from self
-        :return: list of QPolygonF objects returning to self
-        """
-        if exists(path) and splitext(path)[1] in ['.shp', '.gpkg', '.geojson']:
-            ds = ogr.Open(path)
-            layer = ds.GetLayer()
-            for feature in layer:
-                geometry = feature.GetGeometryRef()
-                if geometry.GetGeometryType() not in (ogr.wkbPolygon, ogr.wkbMultiPolygon):
-                    print("Not all geometries are polygons.")
-
-            # Convert geometries to QPolygonF
-            self.polygons = []
-            for feature in layer:
-                geom = feature.GetGeometryRef()
-                qgeom = polygon_to_polygon(geom)
-                if qgeom is not None:
-                    self.polygons.append(qgeom)
-        else:
-            mb_error = QtWidgets.QMessageBox()
-            mb_error.setWindowTitle('Error')
-            mb_error.setText("File not found.")
-            time.sleep(30)
-            sys.exit()
-
-
+            warning_box = QMessageBox()
+            warning_box.setText("Unsupported format")
+            warning_box.setIcon(QMessageBox.Icon.Warning)
+            warning_box.setInformativeText(
+                f"The selected file '{path.basename(filename)}' could not be loaded. It might be an unsupported format")
+            warning_box.exec()
 
     def mousePressEvent(self, e: QMouseEvent):
-        
-        #Get coordinates of q
+        # get cursor position
         x = e.position().x()
         y = e.position().y()
-        
-        #Add new vertex
-        if self.add_vertex:
-                
-            #Create temporary point
-            p = QPointF(x, y)
-            
-            #Add p to polygon
-            self.pol.append(p)
-            
-        #Move q
-        else:
-            self.q.setX(x)
-            self.q.setY(y)
-            
-        #Repaint screen
+
+        # add vertex
+        self.q.setX(x)
+        self.q.setY(y)
+
+        # repaint
         self.repaint()
 
-
-
+    #  function that sets colors
     def paintEvent(self, e: QPaintEvent):
-        #Draw situation
-        
-        #Create new graphic object
+        # create new object
         qp = QPainter(self)
-        
-        #Start drawing
+        # start drawing
         qp.begin(self)
-        
-        #Set graphical attributes
-        qp.setPen(Qt.GlobalColor.black)
-        qp.setBrush(Qt.GlobalColor.yellow)
-        
-        #Draw polygon
-        qp.drawPolygon(self.pol)
-        
-        #Set graphical attributes
+        # set graphical attributes
+        for (result, polygon) in zip(self.results, self.polygons):
+            # set graphical attributes
+            qp.setPen(QPen(Qt.GlobalColor.lightGray))
+            qp.setBrush(Qt.GlobalColor.transparent)
+            if result == 1:
+                qp.setBrush(Qt.GlobalColor.yellow)
+                qp.setPen(Qt.GlobalColor.black)
+            # draw polygon
+            qp.drawPolygon(polygon)
+
+        qp.setPen(QPen(Qt.GlobalColor.black))
         qp.setBrush(Qt.GlobalColor.red)
-        
-        #Draw point
-        r = 10
-        qp.drawEllipse(int(self.q.x()-r), int(self.q.y()-r), 2*r, 2*r)
-        
-        #End drawing
+        if self.add_vertex == True:
+            # draw vertex
+            r = 5
+            qp.drawEllipse(int(self.q.x() - r), int(self.q.y() - r), 2 * r, 2 * r)
+
         qp.end()
-        
-        
+
+    # function that sets the result
+    def setResults(self, results):
+        """
+        Updates self results obtained by algorithm and widget windows.
+        :param results: list of results [boolean]
+
+        """
+        self.results = results
+        self.repaint()
+
+    # function that adds a point or removes a point
     def switchDrawing(self):
-        #Change what will be drawn
-        self.add_vertex = not(self.add_vertex)
-            
-            
+        self.add_vertex = not (self.add_vertex)
+        self.repaint()
+
+    # function that returns analyzed point
     def getQ(self):
-        #Return analyzed point
+        # return analyzed point
         return self.q
-    
-    
-    def getPol(self):
-        # Return analyzed polygon
-        return self.pol
-    
-    
+
+    # function that returns analyzed polygons
+    def getPolygons(self):
+        return self.polygons
+
+    def getResults(self):
+        return self.results
+
+    # clear dynamic variables
     def clearData(self):
-        #Clear polygon
-        self.pol.clear()
-        
-        #Shift point
+        # clear data
+        self.polygons.clear()
+        self.results.clear()
         self.q.setX(-100)
         self.q.setY(-100)
-        
-        #Repaint screen
         self.repaint()
-
